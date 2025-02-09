1c04ff4bbacbd51523f70ef97d5e68c74fe4d683
shrmohit <161517182+shrmohit@users.noreply.github.com>
2025-02-09 09:53:12 +0530

    onClick event handling

--- healthfoodlist/src/components/Items.jsx
+++ healthfoodlist/src/components/Items.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
-function Items(props) {
+function Items({ fooditem }) {
+  const handleBuyClicked = (event) => {
+    console.log(event);
+
+    console.log(`${fooditem} being bought`);
+  };
   return (
     <li
       className="list-group-item kg-item"
-      key={props.fooditem}
+      key={fooditem}
     >
-      <span>{props.fooditem}</span>
+      <span>{fooditem}</span>
       <button
         type="button"
         className="btn btn-info kg-span"
+        // onClick event handling
+        onClick={(event) => handleBuyClicked(event)}
       >
         Buy
       </button>
