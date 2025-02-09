67540ddefb2e055357720610f1929a528cc3948c
shrmohit <161517182+shrmohit@users.noreply.github.com>
2025-02-09 05:30:26 +0530

    btton value not console

--- calcutator-version-one/src/Components/CalculatorButtons.jsx
+++ calcutator-version-one/src/Components/CalculatorButtons.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 function CalculatorButtons({ buttonnumber }) {
+  function handleKeyPress(event) {
+    console.log("The  press value", event.target.value);
+  }
   return (
     <div className="buttonsContainer">
-      {buttonnumber.map((num) => (
-        <button className="cal-button">{num}</button>
+      {buttonnumber.map((num, index) => (
+        <button
+          onClick={handleKeyPress}
+          className="cal-button"
+          key={index}
+        >
+          {num}
+        </button>
       ))}
     </div>
   );
